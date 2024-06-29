a7fcbdf9a9bb8ab7ccaf9b8b672880ef49eeb901
Toynique <garima.hastir@gmail.com>
2024-06-29 12:45:18 +0530

    Update url.jsx

--- src/url/url.jsx
+++ src/url/url.jsx
@@ -1,5 +1,5 @@
-export const Url = "http://localhost:8080"
+//export const Url = "http://localhost:8080"
 export const ImageUrl = "https://doozie-do.cloud"
-// export const Url = "https://doozie-do.cloud"
+ export const Url = "https://boujeetoys.com"
 
 export const awsFileUrl = 'https://teamlans-test.s3.ap-south-1.amazonaws.com'
