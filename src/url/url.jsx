678cb889a5d5cbbd1464489ec4a33e845d1f470c
Toynique <garima.hastir@gmail.com>
2024-06-29 13:28:32 +0530

    Update url.jsx

--- src/url/url.jsx
+++ src/url/url.jsx
@@ -1,4 +1,4 @@
-//export const Url = "http://localhost:8080"
+//  export const Url = "http://localhost:8080"
 export const ImageUrl = "https://doozie-do.cloud"
  export const Url = "https://boujeetoys.com"
 
