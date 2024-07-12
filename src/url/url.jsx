e5b7297e14761cd890014bb480c9e4f4a877b78c
manishkrt <manishkirthalya@gmail.com>
2024-07-12 15:03:59 +0530

    update

--- src/url/url.jsx
+++ src/url/url.jsx
@@ -1,5 +1,5 @@
- export const Url = "http://localhost:8080"
+//  export const Url = "http://localhost:8080"
 // export const ImageUrl = "https://doozie-do.cloud"
-//  export const Url = "https://boujeetoys.com"
+ export const Url = "https://boujeetoys.com"
 
 export const awsFileUrl = 'https://toynique.s3.ap-south-1.amazonaws.com'
